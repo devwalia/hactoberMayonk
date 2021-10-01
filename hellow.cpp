50d577c6b9d0093e5e7f8878e07d5626eab22dac
dev walia <58397422+devwalia@users.noreply.github.com>
2021-10-01 18:19:28 +0530

    Update hellow.cpp

--- hellow.cpp
+++ hellow.cpp
@@ -1,2 +1,15 @@
 
 // this should be the very first line of this projecttt
+// hacktober fest
+#include<iostream>
+ 
+using namespace std;
+ 
+
+int main()
+{
+    
+    cout<<"Hello Naushad";
+     
+    return 0;
+}
